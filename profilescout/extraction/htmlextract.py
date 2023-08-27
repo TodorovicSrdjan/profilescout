TodorovicSrdjan/profilescout

commit a6be4bac8728dfd70ed6149d47c4c8d2585ae589

fix: provide country code in correct format
`phonenumbers`  does not accept the country code in the 'xx-yy' format

profilescout/extraction/htmlextract.py
@@ -353,7 +353,7 @@ def get_resumes(pages, country_code=None):
     # if country_code is not set, try to infer it from html
     if country_code is None:
         country_code = base_page.html.get('lang')
-        country_code = country_code.upper() if country_code is not None else None
+        country_code = country_code.split('-')[0].upper() if country_code is not None else None
 
     # charset = base_page.meta.get('charset')
 

commit 8916481254810a76be5fd18febd73605a6764232

fix: extract country code instead of language code
Fixes: a6be4bac8728dfd70ed6149d47c4c8d2585ae589

profilescout/extraction/htmlextract.py
@@ -355,7 +355,7 @@ def get_resumes(pages, country_code=None):
     # if country_code is not set, try to infer it from html
     if country_code is None:
         country_code = base_page.html.get('lang')
-        country_code = country_code.split('-')[0].upper() if country_code is not None else None
+        country_code = country_code.split('-')[-1].upper() if country_code is not None else None
 
     # charset = base_page.meta.get('charset')
 